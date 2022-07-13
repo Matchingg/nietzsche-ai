400a539402ba20707c2872be3a82d5754dcca1a2
Matthew Ch'ng <mattchng03@gmail.com>
2022-07-13 13:46:19 +1000

    created css

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 
 const App = () => {
+  const instructions =
+    "Simulate readings of Friedrich Nietzsche by generating text that are just as incomprehensible as his own writings!";
+
   const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState("");
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState(instructions);
 
   const handleChange = (event) => {
     const value = event.target.value;
@@ -23,23 +26,29 @@ const App = () => {
   };
 
   return (
-    <div>
-      <h1>Nietzsche AI</h1>
-      <input
-        type="text"
-        onChange={handleChange}
-        placeholder="Input starting text"
-        className="text_input"
-      />
-      <button
-        disabled={isLoading}
-        onClick={!isLoading ? () => handlePredictClick() : null}
-        className="button"
-      >
-        {isLoading ? "Making prediction" : "Predict"}
-      </button>
-      {result && <div>{result}</div>}
-    </div>
+    <>
+      <h1 className="heading">Nietzsche AI</h1>
+      {result && (
+        <div key={result} className="result">
+          {result}
+        </div>
+      )}
+      <div className="container">
+        <input
+          type="text"
+          onChange={handleChange}
+          placeholder="Input starting text"
+          className="text_input"
+        />
+        <button
+          disabled={isLoading}
+          onClick={!isLoading ? () => handlePredictClick() : null}
+          className="button"
+        >
+          {isLoading ? "Making prediction" : "Predict"}
+        </button>
+      </div>
+    </>
   );
 };
 
