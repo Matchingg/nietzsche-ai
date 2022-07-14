67e782ba9653544a765335ab339f9fa40d72740b
Matthew Ch'ng <mattchng03@gmail.com>
2022-07-15 00:25:41 +1000

    removed ports on server

--- server/app.py
+++ server/app.py
@@ -47,5 +47,4 @@ def hello():
         return json.dumps(generated_text)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
